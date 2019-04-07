8f6c34bc2ba98dc1918ad3eebc8a6b393007de11
olehdevops <swatolegntu@gmail.com>
2019-04-07 18:22:45 +0300

    add script8

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-# import tensorflow as tf
-# from sklearn.metrics import explained_variance_score, mean_absolute_error, median_absolute_error
-# from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from sklearn.metrics import explained_variance_score, mean_absolute_error, median_absolute_error
+from sklearn.model_selection import train_test_split
 from flask import Flask, request
 import requests
 import json
